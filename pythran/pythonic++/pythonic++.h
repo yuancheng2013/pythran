d04438be4f589187b4e839adfeb7b8f111899e9c
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2012-12-26 18:24:42 +0100

    [numpy] array c -> python

--- pythran/pythonic++/pythonic++.h
+++ pythran/pythonic++/pythonic++.h
@@ -2,6 +2,7 @@
 #define PYTHONIC_PP_H
 
 /* core */
+#include "core/ndarray.h"
 #include "core/utils.h"
 #include "core/functional.h"
 #include "core/none.h"
@@ -18,6 +19,7 @@
 
 
 /* additional modules */
+#include "modules/numpy.h"
 #include "modules/dispatch.h"
 #include "modules/list.h"
 #include "modules/set.h"
