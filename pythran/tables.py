4a90dd85435586caa4990629c001e7b105c2310c
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2013-02-21 14:28:05 +0100

    Add sin function

--- pythran/tables.py
+++ pythran/tables.py
@@ -194,6 +194,7 @@ modules = {
             },
         "numpy": {
             "array": ConstFunctionIntr(),
+            "sin": ConstFunctionIntr(),
             "zeros": ConstFunctionIntr(),
             "ones": ConstFunctionIntr(),
             "empty": ConstFunctionIntr(),
