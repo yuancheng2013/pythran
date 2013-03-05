d3c228aa4a6f5f2592797d18b0f4e0fdf72cb640
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2013-03-05 23:15:55 +0100

    WIP all

--- pythran/tables.py
+++ pythran/tables.py
@@ -195,6 +195,7 @@ modules = {
             "pmap": ConstFunctionIntr(),
             },
         "numpy": {
+            "all": ConstMethodIntr(),
             "array": ConstFunctionIntr(),
             "sin": ConstFunctionIntr(),
             "zeros": ConstFunctionIntr(),
