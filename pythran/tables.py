223b4f5e894793699584a66b2baf13375f587323
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2013-03-11 13:03:38 +0100

    add tranpose function to numpy

--- pythran/tables.py
+++ pythran/tables.py
@@ -209,6 +209,7 @@ modules = {
             "reshape": ConstMethodIntr(),
             "cumsum": ConstMethodIntr(),
             "sum": ConstMethodIntr(),
+            "transpose": ConstMethodIntr(),
             "max": ConstMethodIntr(),
             "min": ConstMethodIntr(),
             "pi": ScalarIntr(),
