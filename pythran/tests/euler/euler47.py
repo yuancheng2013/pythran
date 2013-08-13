abcb62b3e1634e693a886dbc9a96d4cf141a2605
Yuancheng <yuancheng.peng@telecom-bretagne.eu>
2013-08-13 18:19:28 +0200

    Correct euler47

--- pythran/tests/euler/euler47.py
+++ pythran/tests/euler/euler47.py
@@ -6,6 +6,7 @@ def solve():
     
     prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23]   # Ensure that this is initialised with at least 1 prime
     prime_dict = dict.fromkeys(prime_list, 1)
+    global lastn
     lastn      = prime_list[-1]
     def _isprime(n):
         ''' Raw check to see if n is prime. Assumes that prime_list is already populated '''
@@ -19,6 +20,7 @@ def solve():
         return isprime
     def _refresh(x):
         ''' Refreshes primes upto x '''
+        global lastn
         while lastn <= x:                           # Keep working until we've got up to x
             lastn = lastn + 1                       # Check the next number
             if _isprime(lastn):
@@ -45,4 +47,3 @@ def solve():
         else: factors.append(distinct_factors(len(factors)))
     
     return len(factors)-4
-
